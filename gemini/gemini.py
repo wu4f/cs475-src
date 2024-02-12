2de11dff9c56753d7152306fb5e6aebd2fea305f
Wu-chang Feng <wuchang@pdx.edu>
2024-02-12 11:31:00 -0800

    gemini

--- gemini/gemini.py
+++ gemini/gemini.py
@@ -1,16 +1,4 @@
-import vertexai
-import os
-from vertexai.preview.generative_models import GenerativeModel
-
-project_id = os.getenv("GOOGLE_CLOUD_PROJECT")
-location = "us-west1"
-vertexai.init(project=project_id, location=location)
-
-model = GenerativeModel("gemini-pro")
-
-def get_response(model: GenerativeModel, prompt: str) -> str:
-    response = model.generate_content(prompt)
-    return response.text
-
-prompt = "What are the colors of the rainbow?"
-print(get_response(model, prompt))
+from langchain_google_genai import ChatGoogleGenerativeAI
+chat = ChatGoogleGenerativeAI(model="gemini-pro")
+response = chat.invoke("Write me a haiku about Portland State University")
+print(response.content)
---
+++ gemini/gemini_cloudshell.py
@@ -0,0 +1,16 @@
+import vertexai
+import os
+from vertexai.preview.generative_models import GenerativeModel
+
+project_id = os.getenv("GOOGLE_CLOUD_PROJECT")
+location = "us-west1"
+vertexai.init(project=project_id, location=location)
+
+model = GenerativeModel("gemini-pro")
+
+def get_response(model: GenerativeModel, prompt: str) -> str:
+    response = model.generate_content(prompt)
+    return response.text
+
+prompt = "Give me some sage advice."
+print(get_response(model, prompt))
