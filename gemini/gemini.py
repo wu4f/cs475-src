8fc4952d4594d9dd79b8d6f293a85500af2fca31
Wu-chang Feng <wuchang@pdx.edu>
2024-02-16 10:28:27 -0800

    update requirements for mstic_gemini version

--- gemini/gemini.py
+++ gemini/gemini.py
@@ -1,3 +1,4 @@
+# Requires export GOOGLE_API_KEY=""
 from langchain_google_genai import GoogleGenerativeAI
 llm = GoogleGenerativeAI(model="gemini-pro")
 response = llm.invoke("Write me a haiku about Portland State University")
