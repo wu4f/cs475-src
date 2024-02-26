27aa9157b3d5a37ef2ebc4c4b2a57970cf38337f
Wu-chang Feng <wuchang@pdx.edu>
2024-02-25 18:53:15 -0800

    put tests in codelab

---
+++ 02_Tour/image.py
@@ -0,0 +1,25 @@
+# Requires export GOOGLE_API_KEY=""
+from langchain_core.messages import HumanMessage
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+llm = ChatGoogleGenerativeAI(model="gemini-pro-vision")
+
+def query(llm, query, image_url):
+  input(f"Q: {query} {image_url}")
+  message = HumanMessage(
+      content=[
+          {
+              "type": "text",
+              "text": query,
+          },
+          {   "type": "image_url",
+              "image_url": image_url
+          }
+      ]
+  )
+  response = llm.invoke([message])
+  return response
+
+image_url = 'https://portswigger.net/cms/images/91/43/e4e5-article-popovers-hidden-inputs.png'
+
+print(query(llm, "What is going on in this image?", image_url))
