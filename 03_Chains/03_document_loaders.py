689f4a058367f4f8ad13d12a17204d2c9f2cf154
davidbakerrobinson <dbake2@pdx.edu>
2024-04-05 19:36:39 +0000

    Added another pdf for loader

--- 03_Chains/03_document_loaders.py
+++ 03_Chains/03_document_loaders.py
@@ -5,12 +5,16 @@ from langchain_community.document_loaders import WebBaseLoader
 from langchain_community.document_loaders.recursive_url_loader import RecursiveUrlLoader
 from langchain_community.document_loaders.csv_loader import CSVLoader
 from langchain_community.document_loaders import ArxivLoader
+from langchain_community.document_loaders import WikipediaLoader
+
 
 from langchain.chains import StuffDocumentsChain
 from langchain.chains.llm import LLMChain
 from bs4 import BeautifulSoup as Soup
 from langchain.prompts import PromptTemplate
+import requests
 import argparse
+import os
 
 #Initialize Model
 llm = ChatGoogleGenerativeAI(model="gemini-pro")
@@ -52,27 +56,85 @@ def recursive_loader():
 
 
 def csv_loader():
-    loader = CSVLoader("https://raw.githubusercontent.com/datasets/covid-19/main/data/countries-aggregated.csv")
+    loader = CSVLoader("./rag_data/csv/gemini_generated.csv")
     docs = loader.load()
     for i in docs:
-        print(i.metadata)
+        print(i)
 
-def useful_document_loaders():
-    print("TO BE CONTINUED...")
-    csv_loader()
+def arxiv_loader():
+    loader = ArxivLoader(query="2310.03714", load_max_docs=2)
+    docs = loader.load()
+    len(docs)
+
+    print(docs[0].metadata)
+    print(docs[0].page_content[:200])
+
+def wikipedia_loader():
+    loader = WikipediaLoader(query="LLM Pipelines", load_max_docs=2)
+    docs = loader.load()
+    len(docs)
+
+    print(docs[0].metadata)
+    print(docs[0].page_content[:200])
+
+    print(docs[1].metadata)
+    print(docs[1].page_content[:200])
+
+
+def print_with_hashtag(string):
+    length = len(string)
+    print("#" * (length + 4))
+    print("# " + string + " #")
+    print("#" * (length + 4))
+
+
+def print_chunks(chunkers):
+    for i, chunk in enumerate(chunkers):
+        print_with_hashtag(f"Chunk {i}")
+        print(chunk)
+        print("\n\n" + "-"*80)
+
+def unstructured_print_chunked_pdf(pdf_path):
+    UNSTRUCTURED_API_KEY = os.environ.get('UNSTRUCTURED_API_KEY')
+    if UNSTRUCTURED_API_KEY is None:
+        print("UNSTRUCTURED_API_KEY environment variable not set.")
+        return
+    UNSTRUCTURED_URL = os.environ.get('UNSTRUCTURED_URL')
+    if UNSTRUCTURED_URL is None:
+        print("UNSTRUCTURED_ENDPOINT environment variable not set.")
+        return
+
+    headers = {'accept': 'application/json', 'unstructured-api-key': UNSTRUCTURED_API_KEY }
+    MAX_CHARACTERS = 500
+    COMBINE_UNDER_N_CHARS = 2000
+    strategy = {"strategy":"hi_res"}
+    response = requests.post(UNSTRUCTURED_URL, headers=headers, files={"files": open(pdf_path, "rb")}, data={"strategy": "hi_res" ,"chunking_strategy": "by_title","max_characters": MAX_CHARACTERS, "combine_text_under_n_chars": COMBINE_UNDER_N_CHARS, "overlap": 100})
+    if response.status_code == 200:
+        # process response.json()
+        result = response.json()
+        result = [res["text"] for res in result]
+        print_chunks(result)
+    else:
+        print("Error processing request. Check that the endpoint is correct. Also double check your API key. If it still doesn't work it is possibel that the API key and endpoint is still be activated. Return to exerise later.")
+
+def chunker_example():
+    pdf_path = "./rag_data/pdf/angr-ext.pdf"
+    unstructured_print_chunked_pdf(pdf_path)
+
+
+
+def other_loaders():
+    print("\n")
+    print("arXiv example")
+    print("----------------------------------------")
+    arxiv_loader() 
+    print("\n\n\n\n\n")
+    print("Wikipedia example")
+    print("----------------------------------------")
+    wikipedia_loader()
 
 
 def main_driver():
-    # Define your functions here
-    def function1():
-        print("Function 1 called")
-
-    def function2():
-        print("Function 2 called")
-
-    def function3():
-        print("Function 3 called")
-
     # Create argument parser
     parser = argparse.ArgumentParser(description="Description of your program")
 
@@ -87,10 +149,12 @@ def main_driver():
         article_summarizer_no_chunking()
     elif args.function == "recursive_url":
         recursive_loader()
+    elif args.function == "csv_loader":
+        csv_loader()
     elif args.function == "other_loaders":
-        useful_document_loaders()
+        other_loaders()
     elif args.function == "chunkers":
-        print("Function 4 called")
+        chunker_example()
     elif args.function == "tokenizers":
         print("Function 5 called")
 
