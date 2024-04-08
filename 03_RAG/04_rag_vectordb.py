53aef5000da4ee8f77f81fb7ae06396aef3292f4
Wu-chang Feng <wuchang@pdx.edu>
2024-04-07 21:51:27 -0700

    add wikipedia, arxiv. simplify

--- 03_RAG/04_rag_vectordb.py
+++ 03_RAG/04_rag_vectordb.py
@@ -1,68 +1,77 @@
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.document_loaders import AsyncHtmlLoader, DirectoryLoader, TextLoader, PyPDFDirectoryLoader, Docx2txtLoader, UnstructuredMarkdownLoader
+from langchain_community.document_loaders import AsyncHtmlLoader, DirectoryLoader, TextLoader, PyPDFDirectoryLoader, Docx2txtLoader, UnstructuredMarkdownLoader, WikipediaLoader, ArxivLoader
 from langchain_community.vectorstores import Chroma
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import readline
 
 vectorstore = Chroma(
     embedding_function=GoogleGenerativeAIEmbeddings(model="models/embedding-001", task_type="retrieval_query"),
     persist_directory="./rag_data/.chromadb"
 )
 
-def load_urls(vectorstore, urls):
-    print(f"Loading: {urls}")
-    loader = AsyncHtmlLoader(urls)
-    docs = loader.load()
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=200)
-    splits = text_splitter.split_documents(docs)
-    vectorstore.add_documents(documents=splits)
-
-def load_pdf(vectorstore, directory):
-    print(f"Loading PDF files from: {directory}")
-    loader = PyPDFDirectoryLoader(directory)
-    docs = loader.load()
+def load_docs(docs):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=10)
     splits = text_splitter.split_documents(docs)
     vectorstore.add_documents(documents=splits)
 
-def load_txt(vectorstore, directory):
-    print(f"Loading TXT files from: {directory}")
-    loader = DirectoryLoader(directory, glob="**/*.txt", loader_cls=TextLoader)
-    docs = loader.load()
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=10)
-    splits = text_splitter.split_documents(docs)
-    vectorstore.add_documents(documents=splits)
+def load_urls(urls):
+    load_docs(AsyncHtmlLoader(urls).load())
 
-def load_docx(vectorstore, directory):
-    print(f"Loading DOCX files from: {directory}")
-    loader = DirectoryLoader(directory, glob="**/*.docx", loader_cls=Docx2txtLoader)
-    docs = loader.load()
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=10)
-    splits = text_splitter.split_documents(docs)
-    vectorstore.add_documents(documents=splits)
+def load_wikipedia(query):
+    load_docs(WikipediaLoader(query=query, load_max_docs=1).load())
 
-def load_md(vectorstore, directory):
-    print(f"Loading MD files from: {directory}")
-    loader = DirectoryLoader(directory, glob="**/*.md", loader_cls=UnstructuredMarkdownLoader)
-    docs = loader.load()
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=10)
-    splits = text_splitter.split_documents(docs)
-    vectorstore.add_documents(documents=splits)
+def load_arxiv(query):
+    docs = ArxivLoader(query=query, load_max_docs=1).load()
+    docs[0].metadata['source'] = f"arxiv:{query}"
+    load_docs(docs)
 
-def search_db(vectorstore, query):
+def load_pdf(directory):
+    load_docs(PyPDFDirectoryLoader(directory).load())
+
+def load_txt(directory):
+    load_docs(DirectoryLoader(directory, glob="**/*.txt", loader_cls=TextLoader).load())
+
+def load_docx(directory):
+    load_docs(DirectoryLoader(directory, glob="**/*.docx", loader_cls=Docx2txtLoader).load())
+
+def load_md(directory):
+    load_docs(DirectoryLoader(directory, glob="**/*.md", loader_cls=UnstructuredMarkdownLoader).load())
+
+def search_db(query):
     docs = vectorstore.similarity_search(query)
     print(f"Query database for: {query}")
     if docs:
-        print(f"Closest document match in database: {docs[0].metadata}")
+        print(f"Closest document match in database: {docs[0].metadata['source']}")
+        #print(f"Closest document match in database: {docs[0].metadata}")
     else:
         print("No matching documents")
 
-load_urls(vectorstore, ["https://en.wikipedia.org/wiki/LangChain","https://api.python.langchain.com/en/latest/langchain_api_reference.html","https://en.wikipedia.org/wiki/President_of_the_United_States"])
-load_pdf(vectorstore, "rag_data/pdf")
-load_txt(vectorstore, "rag_data/txt")
-load_docx(vectorstore, "rag_data/docx")
-load_md(vectorstore, "rag_data/md")
-search_db(vectorstore, "How is the President of the United States elected?")
-search_db(vectorstore, "What are data types in Python?")
-search_db(vectorstore, "What is the first ammendment of the Constitution?")
-search_db(vectorstore, "What are some famous quotes from Martin Luther King Jr?")
-search_db(vectorstore, "How do you create a heading in Markdown?")
+print(f"Loading: {urls}")
+load_urls(["https://www.pdx.edu/", "https://www.pdx.edu/computer-science/"])
+print(f"Loading Wikipedia pages on: {query}")
+load_wikipedia("LangChain")
+print(f"Loading arxiv document: {query}")
+load_arxiv("2310.03714")
+print(f"Loading PDF files from: {directory}")
+load_pdf("rag_data/pdf")
+print(f"Loading TXT files from: {directory}")
+load_txt("rag_data/txt")
+print(f"Loading DOCX files from: {directory}")
+load_docx("rag_data/docx")
+print(f"Loading MD files from: {directory}")
+load_md("rag_data/md")
+
+print("RAG database initialized.")
+retriever = vectorstore.as_retriever()
+document_data_sources = set()
+for doc_metadata in retriever.vectorstore.get()['metadatas']:
+    document_data_sources.add(doc_metadata['source']) 
+for doc in document_data_sources:
+    print(f"  {doc}")
+
+while True:
+    line = input("llm>> ")
+    if line:
+        search_db(line)
+    else:
+        break
