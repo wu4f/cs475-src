f1b2bd4f139c3eab93bf138b2b3fecbbb4e5b1dc
Wu-chang Feng <wuchang@pdx.edu>
2024-04-09 15:01:59 -0700

    simple substitution to make more readable

--- 03_RAG/02_recursive_loader.py
+++ 03_RAG/02_recursive_loader.py
@@ -1,24 +1,18 @@
-from langchain_google_genai import GoogleGenerativeAI
 from langchain_community.document_loaders.recursive_url_loader import RecursiveUrlLoader
-from bs4 import BeautifulSoup as Soup
+from bs4 import BeautifulSoup
 import re
-
-llm = GoogleGenerativeAI(model="gemini-pro")
 
 def get_article(page):
    new_page = re.sub('<br />','  ', page)
-   article = Soup(new_page, "html.parser").find('article')
+   article = BeautifulSoup(new_page, "html.parser").find('article')
    if article:
-       return(article.get_text())
+       return(re.sub('\n',' ',article.get_text()))
 
 loader = RecursiveUrlLoader(
-             url = "https://pdx.edu/computer-science/contact",
+             url = "https://pdx.edu/computer-science",
              max_depth = 2,
              extractor=lambda x: get_article(x)
 )
 
 docs = loader.load()
-print(len(docs))
-for d in docs:
-  print(d.metadata)
-  print(d.page_content)
+print(f"Downloaded and parsed {len(docs)} URLs")
