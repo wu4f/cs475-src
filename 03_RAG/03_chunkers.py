e08bda6f17d70ecb99d8f216922d86396c275547
Wu-chang Feng <wuchang@pdx.edu>
2024-04-09 14:59:08 -0700

    re-use 01 code rather than make a copy

--- 03_RAG/03_chunkers.py
+++ 03_RAG/03_chunkers.py
@@ -1,19 +1,12 @@
 from langchain_google_genai import GoogleGenerativeAI
 from langchain_community.document_loaders import AsyncHtmlLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+import importlib
 import time
 
+module = importlib.import_module("01_loaders_transformers")
 llm = GoogleGenerativeAI(model="gemini-pro")
 
-def query_page(content):
-   print(f"Content size: {len(content)}")
-   print(f"Content [:100]: {content[0:100]}")
-   prompt = f"How many undergraduate CS majors are there?  Answer based on the provided context. \n Context: {content}"
-   start = time.time()
-   response = llm.invoke(prompt)
-   end = time.time()
-   print(f"Time elapsed: {end-start} seconds\nResponse: {response}")
-  
 loader = AsyncHtmlLoader("https://pdx.edu/computer-science")
 docs = loader.load()
 text_splitter = RecursiveCharacterTextSplitter(chunk_size = 10000, chunk_overlap=1000)
@@ -24,4 +17,4 @@ article_chunks = [i for i in range(len(docs_splits)) if '<article>' in docs_spli
 
 for i in article_chunks:
     print(f"Found chunk {i} with <article> tag.  Sending to LLM")
-    query_page(docs_splits[i].page_content)
+    module.query_page(docs_splits[i].page_content)
