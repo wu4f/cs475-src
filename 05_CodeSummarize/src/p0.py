4b3478527486adba8b2285b15b6f59743c0463f1
Wu-chang Feng <wuchang@pdx.edu>
2024-05-03 10:34:32 -0700

    remove comments

--- 05_CodeSummarize/src/p0.py
+++ 05_CodeSummarize/src/p0.py
@@ -1,9 +1,7 @@
-# Adds three numbers together and returns the result
 def addition(number1, number2):
   result = number1 + number2
   return result
 
-# Calculates the sum of the areas of two squares of lengths length1 and length2
 def area(length1, length2):
   result = addition(length1*length1,length2*length2)
   return result
