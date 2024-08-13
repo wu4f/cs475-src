e6b1677d44127bbb095e8c2cb96630eb621fbbcf
Wu-chang Feng <wuchang@pdx.edu>
2024-08-13 11:41:16 -0700

    migrate to langchain-chroma

--- 03_RAG/07_rag_loaddb.py
+++ 03_RAG/07_rag_loaddb.py
@@ -1,6 +1,6 @@
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import AsyncHtmlLoader, DirectoryLoader, TextLoader, PyPDFDirectoryLoader, Docx2txtLoader, UnstructuredMarkdownLoader, WikipediaLoader, ArxivLoader, CSVLoader
-from langchain_community.vectorstores import Chroma
+from langchain_chroma import Chroma
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import readline
 
