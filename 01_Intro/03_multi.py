872edb84d4300ed244c9514285e1aca091625d44
Wu-chang Feng <wuchang@pdx.edu>
2024-12-19 08:59:52 -0800

    update 2024

--- 01_Intro/03_multi.py
+++ 01_Intro/03_multi.py
@@ -15,7 +15,6 @@ llms = [
     HuggingFaceEndpoint(repo_id="mistralai/Mistral-7B-Instruct-v0.2", max_new_tokens=128),
     HuggingFaceHub(repo_id="google/flan-t5-small"),
     ChatOpenAI(model='gpt-4o'),
-    ChatGoogleGenerativeAI(model="gemini-pro"),
     ChatGoogleGenerativeAI(model="gemini-1.5-pro"),
     ChatAnthropic(model="claude-3-sonnet-20240229"),
 ]
