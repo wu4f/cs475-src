d593e17e6d56a6cd45157052b8d974bf44dd4779
Wu-chang Feng <wuchang@pdx.edu>
2025-04-14 11:38:15 -0700

    add chainlit

---
+++ 03_RAG/10_chainlit_rag_query.py
@@ -0,0 +1,51 @@
+import os
+import readline
+from langchain import hub
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
+import chainlit as cl
+
+llm = ChatGoogleGenerativeAI(model=os.getenv("GOOGLE_MODEL"))
+
+vectorstore = Chroma(
+     persist_directory="./rag_data/.chromadb",
+     embedding_function=GoogleGenerativeAIEmbeddings(model="models/embedding-001", task_type="retrieval_query")
+)
+
+retriever = vectorstore.as_retriever()
+
+prompt = hub.pull("rlm/rag-prompt")
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+rag_chain = (
+    {"context": retriever | format_docs, "question": RunnablePassthrough()}
+    | prompt
+    | llm
+    | StrOutputParser()
+)
+
+@cl.on_chat_start
+async def on_chat_start():
+    logo = cl.Image(name="logo", display="inline", url="https://codelabs.cs.pdx.edu/images/pdx-cs-logo.png")
+    await cl.Message(content="", elements=[logo]).send()
+
+    welcome_text = (
+        "👋 **Welcome to the PSU Generative Security Chatbot!**\n\n"
+        "Ask me anything from my set of documents.\n\n"
+    )
+    await cl.Message(content=welcome_text).send()
+
+@cl.on_message
+async def on_message(message: cl.Message):
+    user_query = message.content
+    answer = rag_chain.invoke(user_query)
+    await cl.Message(content=answer).send()
+
+if __name__ == "__main__":
+    cl.run()
+
