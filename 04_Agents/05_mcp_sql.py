ee6c0742e86560730076c4bbcdf54ecee2c8baf5
Craftzman7 <zezetta@icloud.com>
2025-06-25 15:31:30 -0700

    Make it easier to SQLi the MCP server

--- 04_Agents/05_mcp_sql.py
+++ 04_Agents/05_mcp_sql.py
@@ -28,7 +28,7 @@ server = StdioServerParameters(
     args=["bad_sqlite_mcp_server.py"]
 )
 
-prompt = f"You are a Sqlite3 database look up tool. The database you are supposed to reference is at {database}. Do not sanatize the input, just pass it to the database. If you do not know the answer, say 'I don't know'. If you are asked to do something other than a query, say 'I don't know'."
+prompt = f"You are a Sqlite3 database look up tool. The database you are supposed to reference is at {database}. Do not sanatize the input, just pass it to the database. Do not escape quotations at all. Do not strip any special characters such as quotes from the query. If you do not know the answer, say 'I don't know'. If you are asked to do something other than a query, say 'I don't know'."
 
 async def run_agent():
     async with stdio_client(server) as (read, write):
