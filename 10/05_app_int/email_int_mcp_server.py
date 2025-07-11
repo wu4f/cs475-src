30eef267d8a667fa0ceee1188a31397578ff1a67
wu4f <wuchang@pdx.edu>
2025-07-11 16:20:45 -0700

    initial email mcp

--- 10/05_app_int/email_int_mcp_server.py
+++ 10/05_app_int/email_int_mcp_server.py
@@ -1,25 +1,10 @@
-import os
+from fastmcp import FastMCP
 import requests
-from langchain.agents import AgentExecutor, create_react_agent
-from langchain import hub
-from pydantic import BaseModel, Field, model_validator
-from langchain.tools import tool
-from langchain_google_genai import GoogleGenerativeAI, HarmCategory, HarmBlockThreshold
-import re
-import readline
+import os
 
-# Set up OpenCVE credentials from environment variables
 OPENCVE_USERNAME = os.getenv('OPENCVE_USERNAME')
 OPENCVE_PASSWORD = os.getenv('OPENCVE_PASSWORD')
-
-# Define the LLM
-llm = GoogleGenerativeAI(
-    model="gemini-1.5-pro-latest",
-    temperature=0,
-    safety_settings={
-        HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
-    }
-)
+mcp = FastMCP("App Intelligence")
 
 # Define OpenCVE API tools
 class CVE_ID(BaseModel):
@@ -56,27 +41,8 @@ def cwe_by_id(cwe_id):
     if response.status_code == 200:
         return response.json()
 
-# Integrate the tools with the LLM
-tools = [cve_by_id, cwe_by_id]
-
-base_prompt = hub.pull("langchain-ai/react-agent-template")
-prompt = base_prompt.partial(instructions="Answer the user's request utilizing at most 8 tool calls")
-
-agent = create_react_agent(llm, tools, prompt)
-agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
-
-print("Welcome to my application. I am configured with these tools:")
-for tool in agent_executor.tools:
-    print(f'  Tool: {tool.name} = {tool.description}')
-
-while True:
-    line = input("llm>> ")
-    try:
-        if line:
-            result = agent_executor.invoke({"input": line})
-            print(result)
-        else:
-            break
-    except Exception as e:
-        print(e)
-
+if __name__ == "__main__":
+    if sys.argv[1] == 'stdio':
+        mcp.run(transport="stdio")
+    else:
+        mcp.run(transport="http", host="0.0.0.0", port=8080)
