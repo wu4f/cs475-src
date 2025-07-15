1ccd16a691b49b82e58805f2fc54f79e44c0c281
Wu-chang Feng <wuchang@pdx.edu>
2025-07-15 11:05:53 -0700

    catch errors

--- 10/07_MSTIC_Agent/mstic_mcp_client.py
+++ 10/07_MSTIC_Agent/mstic_mcp_client.py
@@ -15,7 +15,10 @@ fast = FastAgent("MSTIC Agent")
 async def main():
     async with fast.run() as agent:
         print("Welcome to the MSTIC Agent.  Ask me a query about particular IP addresses and I will examine threat intelligence to generate a report.  Example: Can you give me more details about this ip: 77.246.107.91? How many samples are related to this ip? If you found samples related, can you give me more info about the first one?")
-        await agent.interactive()
+        try:
+            await agent.interactive()
+        except Exception:
+            return
 
 if __name__ == "__main__":
     asyncio.run(main())
