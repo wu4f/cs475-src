455b9036333432887e0a769ee7dc81b185cd4568
Wu-chang Feng <wuchang@pdx.edu>
2025-10-15 15:53:29 -0700

    simplify lab

--- 04_MCP/01_sqlite/fastagent_mcp_client.py
+++ 04_MCP/01_sqlite/fastagent_mcp_client.py
@@ -8,37 +8,10 @@ import questionary
 # Create the application
 fast = FastAgent("SQLite Agent")
 
-choices = []
-for model in ModelFactory.DEFAULT_PROVIDERS:
-    if ModelFactory.DEFAULT_PROVIDERS[model] == Provider.FAST_AGENT:
-        continue
-    choices.append(model)
-
-choices.append("custom")
-
-model = questionary.select(
-    "Select the model to use for the agent:",
-    choices=choices,
-).ask()
-
-if model == "custom":
-    model = questionary.text(
-        "Enter the model name (e.g., 'openai.gpt-4o', 'openai.gpt-4.1', etc.):"
-    ).ask()
-
-tools = []
-for tool in fast.config["mcp"]["servers"]:
-    tools.append(tool)
-
-tools = questionary.checkbox(
-    "Select the tools to use for the agent:",
-    choices=tools,
-).ask()
-
 @fast.agent(
     instruction=f"You are a Sqlite3 database look up tool. Perform queries on the database given the user's input.  Utilize the user input verbatim when sending the query to the database and print the query that was sent to the database",
     model="gpt-4.1",
-    servers=tools,
+    servers=["vulnerable_sqlite_stdio"],
     use_history=True,
 )
 async def main():
