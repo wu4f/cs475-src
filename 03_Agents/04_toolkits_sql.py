5c389a68aadafc528d2697f2d36675724d1b173f
Wu-chang Feng <wuchang@pdx.edu>
2025-12-16 15:58:24 -0800

    executor no longer supported

--- 03_Agents/04_toolkits_sql.py
+++ 03_Agents/04_toolkits_sql.py
@@ -1,7 +1,6 @@
 import os
 import sys
 import readline
-from langchain.agents import AgentExecutor
 from langchain_community.agent_toolkits.sql.base import create_sql_agent
 from langchain_community.agent_toolkits import SQLDatabaseToolkit
 from langchain_community.utilities import SQLDatabase
@@ -19,22 +18,21 @@ llm = ChatGoogleGenerativeAI(
 
 database = sys.argv[1]
 db = SQLDatabase.from_uri(f"sqlite:///{database}")
-toolkit = SQLDatabaseToolkit(db=db,llm=llm)
-agent_executor = create_sql_agent(
+agent = create_sql_agent(
     llm=llm,
-    toolkit=toolkit,
+    db=db,
     verbose=True
 )
 
 print(f"Welcome to my database querying application.  I've loaded your database at {database} and I am configured with these tools:")
-for tool in agent_executor.tools:
+for tool in agent.tools:
   print(f'  Tool: {tool.name} = {tool.description}')
 
 while True:
     line = input("llm>> ")
     if line:
         try:
-            result = agent_executor.invoke(line)
+            result = agent.invoke(line)
             print(result)
         except Exception as e:
             print(e)
